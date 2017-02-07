55037ad8eb852a790fc33ae762a3cfdad8e7c05f
Markus Mayer <widemeadows@gmail.com>
2017-02-07 14:41:13 +0100

    very slow convergence on the "upsampling" network

---
+++ libs/impex.py
@@ -0,0 +1,70 @@
+from os import path
+
+import tensorflow as tf
+from tensorflow.python.framework import graph_util
+
+
+def export_graph(input_path, output_path, output_nodes, debug=False):
+    checkpoint = tf.train.latest_checkpoint(input_path)
+    importer = tf.train.import_meta_graph(checkpoint + '.meta', clear_devices=True)
+
+    graph = tf.get_default_graph()  # type: tf.Graph
+    gd = graph.as_graph_def()  # type: tf.GraphDef
+
+    if debug:
+        op_names = [op.name for op in graph.get_operations()]
+        print(op_names)
+
+    # fix batch norm nodes
+    # https://github.com/tensorflow/tensorflow/issues/3628
+    for node in gd.node:
+        if node.op == 'RefSwitch':
+            node.op = 'Switch'
+            for index in range(len(node.input)):
+                if 'moving_' in node.input[index]:
+                    node.input[index] += '/read'
+        elif node.op == 'AssignSub':
+            node.op = 'Sub'
+            if 'use_locking' in node.attr:
+                del node.attr['use_locking']
+        elif node.op == 'AssignAdd':
+            node.op = 'Add'
+            if 'use_locking' in node.attr:
+                del node.attr['use_locking']
+
+    if debug:
+        print('Freezing the graph ...')
+        with tf.Session() as sess:
+            importer.restore(sess, checkpoint)
+            output_graph_def = graph_util.convert_variables_to_constants(sess, gd, output_nodes)
+            tf.train.write_graph(output_graph_def, path.dirname(output_path), path.basename(output_path), as_text=False)
+
+
+def import_graph(input_path, output_nodes=None, input_map=None, debug=False, use_current_graph=True, prefix='imported'):
+    if debug:
+        print('Reading back the graph ...')
+
+    with tf.gfile.GFile(input_path, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    graph = tf.get_default_graph() if use_current_graph else tf.Graph()
+    with graph.as_default():
+        tensors = tf.import_graph_def(
+                graph_def,
+                input_map=input_map,
+                return_elements=output_nodes,
+                name=prefix,
+                op_dict=None,
+                producer_op_list=None
+            )
+
+    # the output argument corresponds to the tensors defined by return_elements
+    assert (tensors is None and output_nodes is None) or (len(tensors) == len(output_nodes))
+
+    if debug:
+        ops = graph.get_operations()
+        op_names = [op.name for op in ops]
+        print(op_names)
+
+    return tensors
--- train_upsample.py
+++ train_upsample.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import numpy as np
 
 from libs import import_images
+from libs.impex import import_graph
 from libs.utils import conv2d, deconv2d
 
 
@@ -44,44 +45,39 @@ def upscale_bilinear(x):
         return tf.image.resize_bilinear(x, size=size, name='bicubic')
 
 
-def generator(x):
-    with tf.variable_scope('upsample'):
+def generator(x, name='upsample'):
+    with tf.variable_scope(name):
         shape = x.get_shape()
         height = shape[1].value
         width = shape[2].value
-        channels = shape[3].value
+        output_channels = shape[3].value
 
         with tf.variable_scope('residuals_lo'):
-            h = x
-            for i in range(3):
-                h, _ = conv2d(x, n_output=3, d_h=1, d_w=1, name='conv/{}'.format(i))
-                h = tf.nn.elu(h)
+            with tf.variable_scope('prepass'):
+                h, _ = conv2d(x, n_output=32, d_h=1, d_w=1, name='1')
+                h = tf.nn.elu(h, name='1/elu')
+                x = h
+
+                h, _ = conv2d(h, n_output=32, d_h=2, d_w=2, name='2')
+                h = tf.nn.elu(h, name='2/elu')
+
+            channels = [32, 64, 32]
+            for i, c in enumerate(channels):
+                h, _ = conv2d(h, n_output=c, d_h=1, d_w=1, name='conv/{}'.format(i))
+                h = tf.nn.elu(h, name='conv/{}/elu'.format(i))
+
+            h, _ = deconv2d(h,
+                            n_output_h=height,
+                            n_output_w=width,
+                            n_output_ch=32,
+                            name='upscaling')
+            h = tf.nn.elu(h, name='upscaling/elu')
 
             h = tf.add(h, x, name='join')
-            h_lo = h
-
-        with tf.variable_scope('upsample'):
-            h, _ = deconv2d(x,
-                            n_output_h=2*height,
-                            n_output_w=2*width,
-                            n_output_ch=6,
-                            name='deconv')
-            h = tf.nn.elu(h)
-
-        with tf.variable_scope('residuals_hi'):
-            for i in range(2):
-                h, _ = conv2d(h, n_output=6, d_h=1, d_w=1, name='conv/{}'.format(i))
-                h = tf.nn.elu(h)
-
-            h, _ = conv2d(h, n_output=channels, d_h=1, d_w=1, name='conv/prejoin')
-            h = tf.nn.elu(h)
-
-            h_upscaled = upscale_bilinear(h_lo)
-            h = tf.add(h, h_upscaled, name='join')
 
         with tf.variable_scope('clean_pass'):
-            h, _ = conv2d(h, n_output=channels, d_h=1, d_w=1)
-            h = tf.nn.elu(h)
+            h, _ = conv2d(h, n_output=output_channels, d_h=1, d_w=1)
+            h = tf.nn.relu(h, name='relu')
 
         return h
 
@@ -95,12 +91,9 @@ def main():
     batch_size = 50
 
     n_epochs = 50
-    n_epochs_pretrain = 1000
-    keep_prob = 0.8
-    n_code = 512
-    lr_pretrain = 1e-2
+    lr_pretrain = 1e-3
     lr_train = 1e-4
-    img_step = 20
+    img_step = 25
 
     timestamp = datetime.today().strftime('%Y%m%d-%H%M%S')
     log_path = path.join('log.upsample', timestamp)
@@ -111,20 +104,52 @@ def main():
 
         image_batch, type_batch = import_images(tfrecord_file_names, max_reads=max_reads, batch_size=batch_size)
 
-        reference = upscale_bicubic(image_batch)
-        upsampled = generator(image_batch)
+        # todo: use augmentation; requires tf.map_fn() to work across batches
+        # image_batch = tf.image.random_flip_left_right(image_batch)
+        # image_batch = tf.image.random_flip_up_down(image_batch)
+
+        import_graph('exported/vae.pb', input_map={'vae/x': image_batch}, prefix='mogrify')
+        phase_train = graph.get_tensor_by_name('mogrify/vae/phase_train:0')
+        reconstructed = graph.get_tensor_by_name('mogrify/vae/decoder/6/Elu:0')
+        reconstructed.set_shape((None, 180, 320, 3))
+
+        # perform simple clipping
+        reconstructed = tf.nn.relu(reconstructed, name='mogrify/clip')
+
+        refined = generator(reconstructed, name='refine')
 
         with tf.variable_scope('training'):
-            # todo: covariance loss?
-            loss = tf.reduce_mean(tf.reduce_sum(tf.square(upsampled - reference), axis=3))
-            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, global_step)
+            # programmer is using paranoia. it's super effective.
+            # ensure that only the upsampling graph is trained
+            vars = [v for v in tf.trainable_variables() if v.name.startswith('refine')]
+
+            # image should be similar to the original ...
+            loss_1 = tf.reduce_sum(tf.square(refined - image_batch), axis=3)
+            loss_1 = tf.reduce_sum(loss_1, axis=2)
+            loss_1 = tf.reduce_sum(loss_1, axis=1)
+            loss_1 = tf.reduce_mean(loss_1)
+
+            # ... but dissimilar to the VAE reconstruction ...
+            loss_2 = tf.reduce_sum(tf.square(refined - reconstructed), axis=3)
+            loss_2 = tf.reduce_sum(loss_2, axis=2)
+            loss_2 = tf.reduce_sum(loss_2, axis=1)
+            loss_2 = 1.e4/tf.reduce_mean(loss_2)
+
+            # ... and we do both at the same time.
+            loss = 0.6*loss_1 + 0.4*loss_2
+
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)\
+                .minimize(loss, global_step, var_list=vars)
+
+        tf.summary.scalar('loss', loss_1, collections=['test'])
+        tf.summary.scalar('learning_rate', learning_rate, collections=['test'])
+        tf.summary.image('input', image_batch, collections=['test'])
+        tf.summary.image('vae', reconstructed, collections=['test'])
+        tf.summary.image('ref', refined, collections=['test'])
+        test_summaries = tf.summary.merge_all('test')
 
     sv = tf.train.Supervisor(graph=graph, logdir=log_path)
     with sv.managed_session() as sess:
-        # import the VAE metagraph
-        vae_saver = tf.train.import_meta_graph('log/vae/model.ckpt-247304.meta')
-        vae_saver.restore(sess, 'log/vae/model.ckpt-247304')
-
         try:
             print('Loading test data ...')
 
@@ -134,17 +159,32 @@ def main():
             test_Xs = np.array(test_images, np.float32) / 255.
 
             epoch_i = 0
-            while not sv.should_stop() and epoch_i < 1000:
+            loss_value = 1e4
+
+            # TODO: needs to be turned off when continuing learning
+            batch_i = 0
+            while not sv.should_stop() and epoch_i < 1000 and loss_value >= 400:
                 # run pre-training on a small subset of the images (here: the visual test images)
-                batch_i, loss_value, _ = sess.run([global_step, loss, optimizer],
-                                                  feed_dict={learning_rate: lr_pretrain,
-                                                             image_batch: test_Xs})
+                if (batch_i - 1) % img_step == 0:
+                    batch_i, loss_value, s, _ = sess.run([global_step, loss, test_summaries, optimizer],
+                                                         feed_dict={learning_rate: lr_pretrain,
+                                                                    loss_2: 0,
+                                                                    image_batch: test_Xs,
+                                                                    reconstructed: test_Xs})
+                    sv.summary_computed(sess, s, batch_i)
+                else:
+                    batch_i, loss_value, _ = sess.run([global_step, loss, optimizer],
+                                                      feed_dict={learning_rate: lr_pretrain,
+                                                                 loss_2: 0,
+                                                                 image_batch: test_Xs,
+                                                                 reconstructed: test_Xs})
                 print('pretrain', batch_i, loss_value)
                 epoch_i += 1
 
-                if batch_i % img_step == 0:
+                if (batch_i-1) % img_step == 0:
                     # visually evaluate the outcome
-                    resized_inputs, results = sess.run([reference, upsampled], feed_dict={image_batch: test_Xs})
+                    resized_inputs, results = sess.run([image_batch, refined], feed_dict={image_batch: test_Xs,
+                                                                                          reconstructed: test_Xs})
                     assert resized_inputs.shape == results.shape
 
                     canvas = example_gallery(resized_inputs, results)
@@ -152,28 +192,39 @@ def main():
 
                 # display responsiveness
                 if (cv2.waitKey(1) & 0xff) == 27:
+                    print('User requested cancellation.')
                     sv.request_stop()
                     break
 
             epoch_i = 0
-            loss_value = 1.
-            while not sv.should_stop() and epoch_i < n_epochs and loss_value >= 1e-3:
+            while not sv.should_stop() and epoch_i < n_epochs:
                 # run one optimization step
-                batch_i, loss_value, _ = sess.run([global_step, loss, optimizer],
-                                                  feed_dict={learning_rate: lr_train})
+                if (batch_i-1) % img_step == 0:
+                    batch_i, loss_value, s, _ = sess.run([global_step, loss, test_summaries, optimizer],
+                                                         feed_dict={learning_rate: lr_train,
+                                                                    phase_train: False})
+                    sv.summary_computed(sess, s, batch_i)
+                else:
+                    batch_i, loss_value, _ = sess.run([global_step, loss, optimizer],
+                                                      feed_dict={learning_rate: lr_train,
+                                                                 phase_train: False})
                 print('train', batch_i, loss_value)
 
-                if batch_i % img_step == 0:
-                    # visually evaluate the outcome
-                    # resized_inputs, results = sess.run([reference, upsampled], feed_dict={image_batch: test_Xs})
-                    resized_inputs, results = sess.run([reference, upsampled])
+                if (batch_i-1) % img_step == 0:
+                    resized_inputs, results = sess.run([reconstructed,  # reference,
+                                                        refined],
+                                                       feed_dict={phase_train: False})
                     assert resized_inputs.shape == results.shape
+
+                    resized_inputs = resized_inputs[:3]
+                    results = results[:3]
 
                     canvas = example_gallery(resized_inputs, results)
                     cv2.imshow(window, canvas)
 
                 # display responsiveness
                 if (cv2.waitKey(1) & 0xff) == 27:
+                    print('User requested cancellation.')
                     sv.request_stop()
                     break
 
@@ -181,6 +232,7 @@ def main():
             print('Read all examples.')
         finally:
             sv.request_stop()
+            sv.wait_for_stop()
 
         cv2.destroyWindow(window)
 
